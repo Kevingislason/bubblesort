ec86139ead6838f1dea11816512e0b1e5f4a5737
Kevin Gislason <kevingislason@gmail.com>
2018-07-30 14:12:29 -0500

    finished split

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,20 @@
-function bubbleSort(array) {
+let obj = {};
+
+
+obj.bubbleSort = function (array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length - i - 1; j++) {
-      let bubbleLeft = array[j];
-      let bubbleRight = array[j + 1];
-      if (bubbleLeft > bubbleRight) {
-        let temp = bubbleLeft;
-        bubbleLeft = bubbleRight;
-        bubbleRight = temp;
+      if (array[j] > array[j + 1]) {
+        swap.call(this);
       }
     }
   }
   return array;
 }
+
+obj.swap() {
+    let temp = array[j];
+    array[j] = array[j + 1];
+    array[j + 1] = temp;
+}
+
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,8 +1,22 @@
 describe('Bubble Sort', function() {
+  beforeAll(function() {
+    spyOn(obj, 'swap').and.callThrough();
+  });
+
+  it('getting to the center of tootsiepop involves exactly three licks', function() {
+    bubbleSort([8, 5, 9, 1, 6, 7]);
+    expect(obj.swap.calls.count()).toEqual(8);
+  });
+
   it('handles an empty array', function() {
     expect(bubbleSort([])).toEqual([]);
   });
+
+  it('handles an array of 1', function() {
+    expect(bubbleSort([8])).toEqual([8]);
+  });
+
   it('handles an array of numbers', function() {
-    expect(bubbleSort([8, 5, 9, 1, 2, 10, 4])).toEqual([1, 2, 4, 5, 8, 9, 10]);
+    expect(bubbleSort([9, 8, 7, 6, 5])).toEqual([5, 6, 7, 8, 9]);
   });
 });
